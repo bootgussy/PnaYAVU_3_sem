08ed3d9ef89b07eb04a176620a42348f80adbc48
bootgussy <dmitriy.butgusaim@gmail.com>
2024-11-27 23:41:19 +0300

    update #2

--- src/cpp/Order.cpp
+++
@@ -1,9 +0,0 @@
-#include "../header/Order.h"
-
-void Order::addOption(const MenuOption& option)
-{
-    if (&option) {
-        orderedOptions.push_back(option);
-        totalCost += option.getPrice() - (option.getPrice() / 100.0 * option.getDiscount());
-    }
-}
